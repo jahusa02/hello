94532a3b2a7c7c0b247e12d7048681a05920159f
jahusa02 <renachtelik@gmail.com>
2018-10-11 14:28:24 +0200

    Add Img Tag

--- src/login.js
+++ src/login.js
@@ -1,18 +1,25 @@
 function onXingAuthLogin(response) {
   var output;
   var lul;
+  var i;
 
   console.log(response);
 
   if (response.user) {
-    output = 'Successful login for ' + response.user.display_name + " with email: " + response.user.active_email + "<br>" + "The person is working for" + response.user.professional_experience.primary_company;
-    lul = JSON.stringify(response.toString());
+    output = 'Successful login for ' + response.user.display_name + " with email: " + response.user.active_email + "<br>";
   } else if (response.error) {
     output = 'Error: ' + response.error;
   }
 
-  
+
+
 
   document.getElementById('output').innerHTML = output;
-  document.getElementById('lul').innerHTML = lul;
+
+
+  i = document.createElement('img');
+  i.setAttribute("src", response.user.photo_urls.maxi_thumb);
+
+
+  document.getElementById('img').appendChild(i);
 }
